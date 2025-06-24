1f44d9f341dc38737f6d84143e2e02b81d29c3cb
Rolf Heij <rolf_heij@sil.org>
2025-06-24 13:34:36 +0200

    Setup Daisy UI

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div className="card flex flex-col items-center justify-center gap-4">
+        <button className='btn btn-primary w-64 rounded-full' onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
