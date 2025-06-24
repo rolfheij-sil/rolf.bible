852a061a3c0a21b304cd809aa46cec7998d240ee
Rolf Heij <rolf_heij@sil.org>
2025-06-24 18:47:16 +0200

    Restore basic homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logo from '/logo.png'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+     <div className="flex min-h-screen select-none flex-col items-center bg-gradient-to-tr from-teal-500 to-blue-600">
+     
       <div className='flex justify-center items-center gap-4'>
-        <a href="https://vite.dev" target="_blank" rel='noreferrer noopener'>
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel='noreferrer noopener'>
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://rolf.bible" target="_blank" rel='noreferrer noopener'>
+          <img src={logo} className="logo" alt="Rolf.Bible logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Rolf.Bible</h1>
       <div className="card flex flex-col items-center justify-center gap-4">
         <button type='button' className='btn btn-primary w-64 rounded-full' onClick={() => {setCount((count) => count + 1)}}>
           count is {count}
@@ -26,9 +22,9 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Rolf.Bible and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
